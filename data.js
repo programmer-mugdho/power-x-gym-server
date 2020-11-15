c6c0f70f0f5330e5f50c53c8139388ff988e6b0b
programmer-mugdho <programmer.mugdho@gmail.com>
2020-11-16 02:49:34 +0600

    Data Added

---
+++ data.js
@@ -0,0 +1,391 @@
+const chooseUsCards = [
+    {
+        image: 'https://i.ibb.co/MRJDTBk/fitness.png',
+        title: 'Free fitness training',
+        description: 'Free Personal Training Online - we will Design & Manage your personalized free workout plan, exercise & weight loss program!'
+    },
+    {
+        image: 'https://i.ibb.co/8bJ0hTd/tons.png',
+        title: 'Tons of cardio & strength',
+        description: 'Regular cardio exercise can help you lose weight, get better sleep, and reduce your risk for chronic disease.'
+    },
+    {
+        image: 'https://i.ibb.co/52qrrHh/commitment.png',
+        title: 'No commitment memberships',
+        description: 'With our no commitment membership, there is no lengthy minimum term. Enjoy a rolling monthly membership that you can cancel or change at any time, perfect if you want to give us a try.'
+    }
+]
+
+// https://i.ibb.co/MRJDTBk/fitness.png
+// https://i.ibb.co/52qrrHh/commitment.png
+// https://i.ibb.co/8bJ0hTd/tons.png
+
+
+const ourTarget = [
+    {
+        image: 'https://i.ibb.co/FDRGBnF/workout-img.jpg',
+        title: 'progression',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and",
+        bgImage: 'https://i.ibb.co/ymjLjSN/progression-img.jpg'
+    },
+    {
+        image: 'https://i.ibb.co/FDRGBnF/workout-img.jpg',
+        title: 'workout',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and",
+        bgImage: 'https://i.ibb.co/qNxmYQP/work-out.png'
+    },
+    {
+        image: 'https://i.ibb.co/FHh63dN/nutrition.png',
+        title: 'nutrition',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and",
+        bgImage: 'https://i.ibb.co/PGgRTTn/nutrition.jpg'
+    }
+]
+
+// see the messages. "Session chat"
+// background img
+
+// https://i.ibb.co/9twGKtg/progression.png
+// https://i.ibb.co/qNxmYQP/work-out.png
+// https://i.ibb.co/FHh63dN/nutrition.png
+
+// icon link
+
+// https://i.ibb.co/PGgRTTn/nutrition.jpg
+// https://i.ibb.co/ymjLjSN/progression-img.jpg
+// https://i.ibb.co/FDRGBnF/workout-img.jpg
+
+// Zoom opening
+const ourClasses = [
+    {
+        image: 'https://i.ibb.co/Gn7YSzb/psycho.png',
+        title: 'psycho training',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, et asperiores repellendus, amet consequatur accusantium assumenda ullam, laboriosam placeat velit rem sunt? Vitae eaque possimus consectetur alias iusto similique laboriosam quis aut consequatur. Sint, repellendus rerum. Veritatis maiores dolorem animi, ab molestiae libero sed praesentium doloribus! Sequi corporis ipsam quis.",
+        schedules: [
+            {
+                day: 'friday',
+                start: '6:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'saturday',
+                start: '3:00 PM',
+                end: '5:00 PM'
+            },
+            {
+                day: 'monday',
+                start: '8:00 PM',
+                end: '10:00 PM'
+            },
+            {
+                day: 'tuesday',
+                start: '6:00 PM',
+                end: '11:00 PM'
+            },
+            {
+                day: 'thursday',
+                start: '8:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'wednesday',
+                start: '9:00 PM',
+                end: '10:00 PM'
+            }
+        ]
+    },
+    {
+        image: 'https://i.ibb.co/JrVdBVJ/self.jpg',
+        title: 'self defence',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, et asperiores repellendus, amet consequatur accusantium assumenda ullam, laboriosam placeat velit rem sunt? Vitae eaque possimus consectetur alias iusto similique laboriosam quis aut consequatur. Sint, repellendus rerum. Veritatis maiores dolorem animi, ab molestiae libero sed praesentium doloribus! Sequi corporis ipsam quis.",
+        schedules: [
+            {
+                day: 'friday',
+                start: '6:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'saturday',
+                start: '3:00 PM',
+                end: '5:00 PM'
+            },
+            {
+                day: 'monday',
+                start: '8:00 PM',
+                end: '10:00 PM'
+            },
+            {
+                day: 'tuesday',
+                start: '6:00 PM',
+                end: '11:00 PM'
+            },
+            {
+                day: 'thursday',
+                start: '8:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'wednesday',
+                start: '9:00 PM',
+                end: '10:00 PM'
+            }
+        ]
+    },
+    {
+        image: 'https://i.ibb.co/KXZDBnp/advance.jpg',
+        title: 'advance gym',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, et asperiores repellendus, amet consequatur accusantium assumenda ullam, laboriosam placeat velit rem sunt? Vitae eaque possimus consectetur alias iusto similique laboriosam quis aut consequatur. Sint, repellendus rerum. Veritatis maiores dolorem animi, ab molestiae libero sed praesentium doloribus! Sequi corporis ipsam quis.",
+        schedules: [
+            {
+                day: 'friday',
+                start: '6:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'saturday',
+                start: '3:00 PM',
+                end: '5:00 PM'
+            },
+            {
+                day: 'monday',
+                start: '8:00 PM',
+                end: '10:00 PM'
+            },
+            {
+                day: 'tuesday',
+                start: '6:00 PM',
+                end: '11:00 PM'
+            },
+            {
+                day: 'thursday',
+                start: '8:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'wednesday',
+                start: '9:00 PM',
+                end: '10:00 PM'
+            }
+        ]
+    },
+    {
+        image: 'https://i.ibb.co/gP7jJXV/cardio.jpg',
+        title: 'cardio training',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, et asperiores repellendus, amet consequatur accusantium assumenda ullam, laboriosam placeat velit rem sunt? Vitae eaque possimus consectetur alias iusto similique laboriosam quis aut consequatur. Sint, repellendus rerum. Veritatis maiores dolorem animi, ab molestiae libero sed praesentium doloribus! Sequi corporis ipsam quis.",
+        schedules: [
+            {
+                day: 'friday',
+                start: '6:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'saturday',
+                start: '3:00 PM',
+                end: '5:00 PM'
+            },
+            {
+                day: 'monday',
+                start: '8:00 PM',
+                end: '10:00 PM'
+            },
+            {
+                day: 'tuesday',
+                start: '6:00 PM',
+                end: '11:00 PM'
+            },
+            {
+                day: 'thursday',
+                start: '8:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'wednesday',
+                start: '9:00 PM',
+                end: '10:00 PM'
+            }
+        ]
+    },
+    {
+        image: 'https://i.ibb.co/2PfNz8V/strength.jpg',
+        title: 'strength training',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, et asperiores repellendus, amet consequatur accusantium assumenda ullam, laboriosam placeat velit rem sunt? Vitae eaque possimus consectetur alias iusto similique laboriosam quis aut consequatur. Sint, repellendus rerum. Veritatis maiores dolorem animi, ab molestiae libero sed praesentium doloribus! Sequi corporis ipsam quis.",
+        schedules: [
+            {
+                day: 'friday',
+                start: '6:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'saturday',
+                start: '3:00 PM',
+                end: '5:00 PM'
+            },
+            {
+                day: 'monday',
+                start: '8:00 PM',
+                end: '10:00 PM'
+            },
+            {
+                day: 'tuesday',
+                start: '6:00 PM',
+                end: '11:00 PM'
+            },
+            {
+                day: 'thursday',
+                start: '8:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'wednesday',
+                start: '9:00 PM',
+                end: '10:00 PM'
+            }
+        ]
+    },
+    {
+        image: 'https://i.ibb.co/cxz46CB/push-ups.jpg',
+        title: 'push ups training',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, et asperiores repellendus, amet consequatur accusantium assumenda ullam, laboriosam placeat velit rem sunt? Vitae eaque possimus consectetur alias iusto similique laboriosam quis aut consequatur. Sint, repellendus rerum. Veritatis maiores dolorem animi, ab molestiae libero sed praesentium doloribus! Sequi corporis ipsam quis.",
+        schedules: [
+            {
+                day: 'friday',
+                start: '6:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'saturday',
+                start: '3:00 PM',
+                end: '5:00 PM'
+            },
+            {
+                day: 'monday',
+                start: '8:00 PM',
+                end: '10:00 PM'
+            },
+            {
+                day: 'tuesday',
+                start: '6:00 PM',
+                end: '11:00 PM'
+            },
+            {
+                day: 'thursday',
+                start: '8:00 PM',
+                end: '9:00 PM'
+            },
+            {
+                day: 'wednesday',
+                start: '9:00 PM',
+                end: '10:00 PM'
+            }
+        ]
+    }
+]
+// https://us04web.zoom.us/j/78775635183?pwd=dDNtbUJ0TUFCR1dLNzJDUUdUeSsrQT09
+
+// our class photos
+// https://i.ibb.co/3NTPVkv/advance.jpg
+// https://i.ibb.co/CwKDbSX/cardio.jpg
+// https://i.ibb.co/Jj0QHMs/psycho.png
+// https://i.ibb.co/30J7ct0/push-ups.jpg
+// https://i.ibb.co/3mVbDvH/self.jpg
+// https://i.ibb.co/BLg2GgV/strength.jpg
+
+
+// const classSchedule = [
+//     {
+//         title: 'psycho training',
+//         schedules: [
+//             {
+//                 day: 'friday',
+//                 start: '6:00 PM',
+//                 end: '9:00 PM'
+//             },
+//             {
+//                 day: 'saturday',
+//                 start: '3:00 PM',
+//                 end: '5:00 PM'
+//             },
+//             {
+//                 day: 'monday',
+//                 start: '8:00 PM',
+//                 end: '10:00 PM'
+//             },
+//             {
+//                 day: 'tuesday',
+//                 start: '6:00 PM',
+//                 end: '11:00 PM'
+//             },
+//             {
+//                 day: 'thursday',
+//                 start: '8:00 PM',
+//                 end: '9:00 PM'
+//             },
+//             {
+//                 day: 'wednesday',
+//                 start: '9:00 PM',
+//                 end: '10:00 PM'
+//             }
+//         ]
+//     },
+//     {
+//         title: 'self defence',
+//         schedules: [
+//             {
+//                 day: 'friday',
+//                 start: '6:00 PM',
+//                 end: '9:00 PM'
+//             },
+//             {
+//                 day: 'saturday',
+//                 start: '3:00 PM',
+//                 end: '5:00 PM'
+//             },
+//             {
+//                 day: 'monday',
+//                 start: '8:00 PM',
+//                 end: '10:00 PM'
+//             },
+//             {
+//                 day: 'tuesday',
+//                 start: '6:00 PM',
+//                 end: '11:00 PM'
+//             },
+//             {
+//                 day: 'thursday',
+//                 start: '8:00 PM',
+//                 end: '9:00 PM'
+//             },
+//             {
+//                 day: 'wednesday',
+//                 start: '9:00 PM',
+//                 end: '10:00 PM'
+//             }
+//         ]
+//     },
+
+    // {
+    //     "id": 1,
+    //     "name": "Leanne Graham",
+    //     "username": "Bret",
+    //     "email": "Sincere@april.biz",
+    //     "address": {
+    //         "street": "Kulas Light",
+    //         "suite": "Apt. 556",
+    //         "city": "Gwenborough",
+    //         "zipcode": "92998-3874",
+    //         "geo": {
+    //             "lat": "-37.3159",
+    //             "lng": "81.1496"
+    //         }
+    //     },
+    //     "phone": "1-770-736-8031 x56442",
+    //     "website": "hildegard.org",
+    //     "company": {
+    //         "name": "Romaguera-Crona",
+    //         "catchPhrase": "Multi-layered client-server neural-net",
+    //         "bs": "harness real-time e-markets"
+    //     }
+    // }
+
+// ]
+
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req,res)=>{
-    res.send('Hello Node')
+app.get('/', (req, res) => {
+    res.send('Hello Node.Js React js ')
 })
 
 app.listen(process.env.PORT || port)
