4006eff611a004f9677ea75f0266063ed40fc9a5
programmer-mugdho <programmer.mugdho@gmail.com>
2020-11-17 19:23:13 +0600

    Payment API added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://powerXuser:L055KJ2BfcTvB0dh@cluster0.yf6o8.mongodb.net/powerXGym?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const port = 5000;
+
 app.use(cors())
+app.use(bodyParser.json())
 
 // mongoDB
 //ourTarget ,chooseUs
@@ -14,14 +17,16 @@ client.connect(err => {
     const ourTargetCollection = client.db("powerXGym").collection("ourTarget");
     const chooseUsCollection = client.db("powerXGym").collection("chooseUs");
     const ourClassesCollection = client.db("powerXGym").collection("ourClasses");
+    const paymentCollection = client.db("powerXGym").collection("payment");
 
-    app.get('/', (req, res)=>{
+
+    app.get('/', (req, res) => {
         res.send('<h1>Power X Gym Server</h1>')
     })
 
-    app.get('/targets', (req, res)=>{
+    app.get('/targets', (req, res) => {
         ourTargetCollection.find({})
-            .toArray((err, documents)=>{
+            .toArray((err, documents) => {
                 res.send(documents)
             })
     })
@@ -39,13 +44,12 @@ client.connect(err => {
                 res.send(documents)
             })
     })
-    // app.get('/addData', (req, res) => {
-    //     ourClassesCollection.insertMany()
-    //         .then(result => {
-    //             res.send(result.insertedCount > 0);
-    //             console.log(result);
-    //         })
-    // })
+
+    app.post('/payment', (req, res) => {
+        data = req.body
+        paymentCollection.insertOne({ data })
+            .then(result => res.send(result.insertedCount > 0))
+    })
     console.log('Database Connected');
 });
 
